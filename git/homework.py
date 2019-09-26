d49719da390b9abc802606f24af373985bda9562
AnastasiiaButova <but.an90@gmail.com>
2019-09-26 10:09:54 +0300

    add my homework

--- git/homework.py
+++ git/homework.py
@@ -10,31 +10,43 @@ class OurAwesomeException(Exception):
     pass
 
 
-def is_two_object_has_same_value(first: Any, second: Any) -> bool:
+def is_two_object_has_same_value(a,b) -> bool:
     """
     If @first and @second has same value should return True
     In another case should return False
     """
+    if a == b:
+        return True
+    else:
+        return False
     pass
 
 
-def is_two_objects_has_same_type(first: Any, second: Any) -> bool:
+def is_two_objects_has_same_type(a,b) -> bool:
     """
     If @first and @second has same type should return True
     In another case should return False
     """
-    pass
+    if type(a) == type(b):
+        return True
+    else:
+        return False
 
 
-def is_two_objects_is_the_same_objects(first: Any, second: Any) -> bool:
+
+def is_two_objects_is_the_same_objects(a,b) -> bool:
     """
     If @first and @second has same type should return True
     In another case should return False
     """
-    pass
+    if a is b:
+        return True
+    else:
+        return False
 
 
-def multiple_ints(first_value: int, second_value: int) -> int:
+
+def multiple_ints(a,b) -> int:
     """
     Should calculate product of all args.
     if first_value or second_value is not int should raise ValueError
@@ -48,10 +60,14 @@ def multiple_ints(first_value: int, second_value: int) -> int:
     Returns:
         Product of elements
     """
+    if type(a) != int or type(b) != int:
+        raise ValueError('Alarma! Something is not an integer')
+    else:
+        return a * b
     pass
 
 
-def multiple_ints_with_conversion(first_value: Any, second_value: Any) -> int:
+def multiple_ints_with_conversion(a,b) -> int:
     """
     If possible to convert arguments to int value - convert and multiply them.
     If it is impossible raise ValueError
@@ -78,10 +94,22 @@ def multiple_ints_with_conversion(first_value: Any, second_value: Any) -> int:
             print("Not valid input data")
         >>> "Not valid input data"
     """
+    if type((a) is float or int) and (type(b) is float or int):
+        a_int = int(a)
+        b_int = int(b)
+        print(a_int * b_int)
+    elif a.isdigit() or b.isdigit():
+        a = float(a)
+        b = float(b)
+        a_int = int(a)
+        b_int = int(b)
+        print(a_int * b_int)
+    else:
+        raise ValueError('Not valid input data')
     pass
 
 
-def is_word_in_text(word: str, text: str) -> bool:
+def is_word_in_text(text = " ") -> bool:
     """
     If text contain word return True
     In another case return False.
@@ -97,6 +125,11 @@ def is_word_in_text(word: str, text: str) -> bool:
         >>> False
 
     """
+    i = type(str)
+    if 'i' in text:
+        return True
+    else:
+        return False
     pass
 
 
@@ -104,6 +137,10 @@ def some_loop_exercise() -> list:
     """
     Use loop to create list that contain int values from 0 to 12 except 6 and 7
     """
+    l = list(range(12))
+    del l[5:7]
+    for i in l:
+        print(i + 1)
     pass
 
 
@@ -116,6 +153,11 @@ def remove_from_list_all_negative_numbers(data: List[int]) -> list:
         remove_from_list_all_negative_numbers([1, 5, -7, 8, -1])
         >>> [1, 5, 8]
     """
+    l = []
+    for i in l:
+        if i < 0:
+            l.remove(i)
+    print(l)
     pass
 
 
@@ -127,6 +169,13 @@ def alphabet() -> dict:
         alphabet()
         >>> {"a": 1, "b": 2 ...}
     """
+    import string
+    i = 0
+    for c in string.ascii_lowercase:
+        i = i + 1
+        d = {i: c}
+        print(d)
+    pass
     pass
 
 
@@ -139,4 +188,11 @@ def simple_sort(data: List[int]) -> List[list]:
     Returns:
 
     """
-    pass
+    l = []
+    for i in range(len(l)):
+        for j in range(i + 1, len(l)):
+            if l[i] > l[j]:
+                l[i], l[j] = l[j], l[i]
+    print(l)
+
+    print("Done!")
