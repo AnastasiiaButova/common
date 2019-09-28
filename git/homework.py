9a7fa4170069386ecb74e8977879130b061db5aa
AnastasiiaButova <but.an90@gmail.com>
2019-09-28 22:10:09 +0300

    merge

--- git/homework.py
+++ git/homework.py
@@ -10,43 +10,31 @@ class OurAwesomeException(Exception):
     pass
 
 
-def is_two_object_has_same_value(a,b) -> bool:
+def is_two_object_has_same_value(a, b) -> bool:
     """
     If @first and @second has same value should return True
     In another case should return False
     """
-    if a == b:
-        return True
-    else:
-        return False
-    pass
+    return a == b
 
 
-def is_two_objects_has_same_type(a,b) -> bool:
+def is_two_objects_has_same_type(a, b) -> bool:
     """
     If @first and @second has same type should return True
     In another case should return False
     """
-    if type(a) == type(b):
-        return True
-    else:
-        return False
+    return type(a) == type(b)
 
 
-
-def is_two_objects_is_the_same_objects(a,b) -> bool:
+def is_two_objects_is_the_same_objects(a, b) -> bool:
     """
     If @first and @second has same type should return True
     In another case should return False
     """
-    if a is b:
-        return True
-    else:
-        return False
+    return a is b
 
 
-
-def multiple_ints(a,b) -> int:
+def multiple_ints(a, b) -> int:
     """
     Should calculate product of all args.
     if first_value or second_value is not int should raise ValueError
@@ -60,14 +48,13 @@ def multiple_ints(a,b) -> int:
     Returns:
         Product of elements
     """
-    if type(a) != int or type(b) != int:
-        raise ValueError('Alarma! Something is not an integer')
-    else:
-        return a * b
-    pass
+    try:
+        return int(a * b)
+    except:
+        raise ValueError
 
 
-def multiple_ints_with_conversion(a,b) -> int:
+def multiple_ints_with_conversion(a, b) -> int:
     """
     If possible to convert arguments to int value - convert and multiply them.
     If it is impossible raise ValueError
@@ -94,22 +81,13 @@ def multiple_ints_with_conversion(a,b) -> int:
             print("Not valid input data")
         >>> "Not valid input data"
     """
-    if type((a) is float or int) and (type(b) is float or int):
-        a_int = int(a)
-        b_int = int(b)
-        print(a_int * b_int)
-    elif a.isdigit() or b.isdigit():
-        a = float(a)
-        b = float(b)
-        a_int = int(a)
-        b_int = int(b)
-        print(a_int * b_int)
-    else:
+    try:
+        return int(a) * int(b)
+    except:
         raise ValueError('Not valid input data')
-    pass
 
 
-def is_word_in_text(text = " ") -> bool:
+def is_word_in_text(word, text) -> bool:
     """
     If text contain word return True
     In another case return False.
@@ -125,40 +103,27 @@ def is_word_in_text(text = " ") -> bool:
         >>> False
 
     """
-    i = type(str)
-    if 'i' in text:
-        return True
-    else:
-        return False
-    pass
+
+    return word in text
 
 
 def some_loop_exercise() -> list:
     """
     Use loop to create list that contain int values from 0 to 12 except 6 and 7
     """
-    l = list(range(12))
-    del l[5:7]
-    for i in l:
-        print(i + 1)
-    pass
+    return [i for i in range(13) if i not in (6, 7)]
 
 
-def remove_from_list_all_negative_numbers(data: List[int]) -> list:
+def remove_from_list_all_negative_numbers(l: List[int]) -> list:
     """
     Use loops to solve this task.
     You could use data.remove(negative_number) to solve this issue.
     Also you could create new list with only positive numbers.
     Examples:
         remove_from_list_all_negative_numbers([1, 5, -7, 8, -1])
-        >>> [1, 5, 8]
+    #    >>> [1, 5, 8]
     """
-    l = []
-    for i in l:
-        if i < 0:
-            l.remove(i)
-    print(l)
-    pass
+    return [i for i in l if i >= 0]
 
 
 def alphabet() -> dict:
@@ -170,13 +135,10 @@ def alphabet() -> dict:
         >>> {"a": 1, "b": 2 ...}
     """
     import string
-    i = 0
-    for c in string.ascii_lowercase:
-        i = i + 1
-        d = {i: c}
-        print(d)
-    pass
-    pass
+    d = {}
+    for i, c in enumerate(string.ascii_lowercase, 1):
+        d[i] = c
+    return d
 
 
 def simple_sort(data: List[int]) -> List[list]:
@@ -188,11 +150,8 @@ def simple_sort(data: List[int]) -> List[list]:
     Returns:
 
     """
-    l = []
-    for i in range(len(l)):
-        for j in range(i + 1, len(l)):
-            if l[i] > l[j]:
-                l[i], l[j] = l[j], l[i]
-    print(l)
-
-    print("Done!")
+    for i in range(len(data)):
+        for j in range(i + 1, len(data)):
+            if data[i] > data[j]:
+                data[i], data[j] = data[j], data[i]
+    return data
