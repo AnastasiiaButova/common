a8f42a03e5c80ec20b1cba65a0d28004f31e144d
AnastasiiaButova <but.an90@gmail.com>
2019-09-29 16:19:06 +0300

    коміт домашки

--- git/homework.py
+++ git/homework.py
@@ -18,12 +18,14 @@ def is_two_object_has_same_value(a, b) -> bool:
     return a == b
 
 
+
 def is_two_objects_has_same_type(a, b) -> bool:
     """
     If @first and @second has same type should return True
     In another case should return False
     """
     return type(a) == type(b)
+
 
 
 def is_two_objects_is_the_same_objects(a, b) -> bool:
@@ -34,12 +36,15 @@ def is_two_objects_is_the_same_objects(a, b) -> bool:
     return a is b
 
 
+
 def multiple_ints(a, b) -> int:
     """
     Should calculate product of all args.
     if first_value or second_value is not int should raise ValueError
+    
     Raises:
         ValueError
+    
     Params:
         first_value: value for multiply
         second_value
@@ -52,16 +57,21 @@ def multiple_ints(a, b) -> int:
         return a * b
 
 
+
 def multiple_ints_with_conversion(a, b) -> int:
     """
     If possible to convert arguments to int value - convert and multiply them.
     If it is impossible raise ValueError
+    
     Args:
         first_value: number for multiply
         second_value: number for multiply
+    
     Raises:
         ValueError
+    
     Returns: multiple of two numbers.
+    
     Examples:
         multiple_ints_with_conversion(6, 6)
         >>> 36
@@ -81,21 +91,26 @@ def multiple_ints_with_conversion(a, b) -> int:
         raise ValueError('Integer is expected')
 
 
+
 def is_word_in_text(word, text) -> bool:
     """
     If text contain word return True
     In another case return False.
+    
     Args:
         word: Searchable substring
         text: Text for searching
+    
     Examples:
         is_word_in_text("Hello", "Hello word")
         >>> True
         is_word_in_text("Glad", "Nice to meet you ")
         >>> False
+    
     """
 
     return word in text
+
 
 
 def some_loop_exercise() -> list:
@@ -117,6 +132,7 @@ def remove_from_list_all_negative_numbers(l: List[int]) -> list:
     return [i for i in l if i >= 0]
 
 
+
 def alphabet() -> dict:
     """
     Create dict which keys is alphabetic characters. And values their number in alphabet
@@ -132,6 +148,7 @@ def alphabet() -> dict:
     return d
 
 
+
 def simple_sort(data: List[int]) -> List[list]:
     """
     Sort list of ints without using built-in methods.
@@ -139,9 +156,12 @@ def simple_sort(data: List[int]) -> List[list]:
         simple_sort([2, 9, 6, 7, 3, 2, 1])
         >>> [1, 2, 2, 3, 6, 7, 9]
     Returns:
+    
     """
     for i in range(len(data)):
         for j in range(i + 1, len(data)):
             if data[i] > data[j]:
                 data[i], data[j] = data[j], data[i]
     return data
+
+    print("Done!")
