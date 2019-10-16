50a908631f3de505a0206d05c01bbfa5114a2621
AnastasiiaButova <but.an90@gmail.com>
2019-10-16 21:06:17 +0300

    homework.py

--- tests/homework_5.py
+++ tests/homework_5.py
@@ -1,9 +1,5 @@
 import math
 
-
-#a =[1,1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
-#b =[1,2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
-
 def code_two_list(list_1, list_2):
     c = set(list_1) & set(list_2)
     return list(c)
