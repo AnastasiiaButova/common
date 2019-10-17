c5684b77323c53289973cdacb87f66b8dbea556e
AnastasiiaButova <but.an90@gmail.com>
2019-10-17 23:35:58 +0300

    pytests.py

--- tests/pytests.py
+++ tests/pytests.py
@@ -1,7 +1,8 @@
 import math
 import string
 import pytest
-
+import mock
+import builtins
 
 '''Task 11:
 Write a program that will take the num parameter being passed and return the number of hours and minutes
@@ -31,11 +32,11 @@ def longest_word(sentence):
     word = sentence.split(' ')
     return (max(word, key=len))
 
-def test_1_longest_word():
-    assert longest_word('fun&!! time') == 'time'
+@pytest.mark.parametrize("test_input,expected",
+                         [('fun&!! time', 'time'), ('I love dogs', 'love')])
+def test_longest_word(test_input,expected):
+    assert longest_word(test_input) == expected
 
-def test_2_longest_word():
-    assert longest_word('I love dogs') == 'love'
 
 '''Task 13:
 Write a program (using functions!) that asks the user for a long string containing multiple words. 
@@ -46,11 +47,45 @@ Input: My name is Michele
 Outout: Michele is name My'''
 
 
-def backwards(sentence):
+def backwards():
     sentence = input('Please, enter the sentence: ')
-    sentence = sentence.split(' ')
-    output = ' '.join(sentence)
-    return output
+    for line in sentence.split('\n'):
+        return ' '.join(line.split()[::-1])
+
 
 def test_backwards():
-    assert backwards('My name is Michele') == 'Michele is name My'
+    with mock.patch.object(builtins, 'input', lambda _: 'My name is Michele'):
+        assert backwards() == 'Michele is name My'
+
+
+'''Task 14:       
+Write a program that asks the user how many Fibonnaci numbers to generate and then generates them. 
+Take this opportunity to think about how you can use functions. 
+Make sure to ask the user to enter the number of numbers in the sequence to generate.'''
+
+
+def fibonacci():
+    numbers = int(input('How many Fibonacci numbers to generate : '))
+    a, b = 0, 1
+    while b < numbers:
+        a, b = b, a + b
+        return b
+
+
+#def test_fibonnaci():
+#    with pytest.raises(ValueError):
+#        fibonnaci(-1)
+
+def test_fibonacci():
+    with mock.patch('builtins, input', return_value = 6):
+        assert fibonacci() == [1, 1, 2, 3, 5, 8]
+
+
+
+
+#class FibonacciTests(TestCase):
+#    def test_returns_correct_fibonacci_number(self):
+#        correct_sequence = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144]
+#        for index in range(len(correct_sequence)):
+#            response = fibonacci(index)
+#            assert response == correct_sequence[index]
