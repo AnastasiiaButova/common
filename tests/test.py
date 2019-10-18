b861ecf48470873b8519597b564469ff4f53651f
AnastasiiaButova <but.an90@gmail.com>
2019-10-18 13:15:31 +0300

    test.py

--- tests/test.py
+++ tests/test.py
@@ -8,47 +8,47 @@ class MyTestCase(unittest.TestCase):
         rectangle = Rectangle(3,4)
         self.assertTrue(True)
 
-    def test_rectangle_perimeter(self, width, height):
-        perimeter = Rectangle(3,4)
-        self.assertEqual(Rectangle.get_rectangle_perimeter(perimeter), 14)
+    def test_rectangle_perimeter(self):
+        rectangle = Rectangle(3,4)
+        self.assertEqual(Rectangle.get_rectangle_perimeter(rectangle), 14)
 
 
     def test_rectangle_square(self):
-        square = Rectangle(3,4)
-        self.assertEqual(Rectangle.get_rectangle_square(square), 12)
+        rectangle = Rectangle(3,4)
+        self.assertEqual(Rectangle.get_rectangle_square(rectangle), 12)
 
 
     def test_sum_of_corners_true(self):
-        sum_of_corners = Rectangle(3, 4)
-        self.assertEqual(Rectangle.get_sum_of_corners(sum_of_corners, 1), 90)
-        self.assertEqual(Rectangle.get_sum_of_corners(sum_of_corners, 2), 180)
-        self.assertEqual(Rectangle.get_sum_of_corners(sum_of_corners, 3), 270)
-        self.assertEqual(Rectangle.get_sum_of_corners(sum_of_corners, 4), 360)
+        rectangle = Rectangle(3, 4)
+        self.assertEqual(Rectangle.get_sum_of_corners(rectangle, 1), 90)
+        self.assertEqual(Rectangle.get_sum_of_corners(rectangle, 2), 180)
+        self.assertEqual(Rectangle.get_sum_of_corners(rectangle, 3), 270)
+        self.assertEqual(Rectangle.get_sum_of_corners(rectangle, 4), 360)
 
     def test_sum_of_corners_false(self):
-        sum_of_corners = Rectangle(3,4)
+        rectangle = Rectangle(3,4)
         with self.assertRaises(ValueError):
-            Rectangle.get_sum_of_corners(sum_of_corners, 5)
+            Rectangle.get_sum_of_corners(rectangle, 5)
 
 
     def test_rectangle_diagonal(self):
-        diagonal = Rectangle(3, 4)
-        self.assertEqual(Rectangle.get_rectangle_diagonal(diagonal), 5)
+        rectangle = Rectangle(3, 4)
+        self.assertEqual(Rectangle.get_rectangle_diagonal(rectangle), 5)
 
 
     def test_radius_of_circumscribed_circle(self):
-        radius = Rectangle(3, 4)
-        self.assertEqual(Rectangle.get_radius_of_circumscribed_circle(radius), 2.5)
+        rectangle = Rectangle(3, 4)
+        self.assertEqual(Rectangle.get_radius_of_circumscribed_circle(rectangle), 2.5)
 
 
     def test_radius_of_inscribed_circle(self):
-        radius = Rectangle(14, 14)
-        self.assertEqual(Rectangle.get_radius_of_inscribed_circle(radius), 7)
+        rectangle = Rectangle(14, 14)
+        self.assertEqual(Rectangle.get_radius_of_inscribed_circle(rectangle), 7)
 
     def test_radius_of_inscribed_circle_false(self):
-        radius = Rectangle(4,3)
+        rectangle = Rectangle(4,3)
         with self.assertRaises(ValueError):
-            Rectangle.get_radius_of_inscribed_circle(radius)
+            Rectangle.get_radius_of_inscribed_circle(rectangle)
 
 
 if __name__ == '__main__':
