c31c6ff47a95f5dbbe5046e35343b83cfcf3d31d
AnastasiiaButova <but.an90@gmail.com>
2019-10-25 22:36:33 +0300

    flask_homework.py

---
+++ flask/flask_homework.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template
+first = Flask(__name__)
+
+
+@first.route('/base')
+def get_base():
+    return render_template('base.html')
+
+
+@first.route('/home')
+def get_home():
+    return render_template('home.html')
+
+
+@first.route('/vegetables')
+def get_vegetables():
+    return render_template('vegetables.html',list_of_vegetables =  ['beans', 'carrot', 'beetroot', 'cucumber'])
+
+
+@first.route('/fruits')
+def get_fruits():
+    return render_template('fruits.html')
+
+
+
+if __name__ == "__main__":
+    first.run(debug=True)
